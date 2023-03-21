63bb270c47bad031b811d4c1503ce59d81f8b56f
AndrewL777 <7landrew7@gmail.com>
2023-03-21 14:00:27 -0400

    4234234

--- vue project/src/App.vue
+++ vue project/src/App.vue
@@ -62,15 +62,26 @@ export default {
   }
 }
 </script>
+<style>
+.card {
+  width: 35.4rem;
+  height: 23.4rem;
+
+  border: 3px solid black;
+  box-shadow: 5px 5px 15px;
+}
+</style>
 
 <template>
   <header>
-    <button>1231</button>
-    <li v-for="pokemon in pokemons" in>{{ pokemon.name }} <img :src="pokemon.url" /></li>
+    <h1>Pick</h1>
+    <div class="card" v-for="pokemon in pokemons">
+      {{ pokemon.name }} <img :src="pokemon.url" />
+    </div>
   </header>
 </template>
 
-<!-- data() { return { items: [{ message: 'Foo' }, { message: 'Bar' }] } } template
+<!-- data() { return { items: [{ message: 'Foo' },{ message: 'Bar' }] } } template
 <li v-for="item in items">
   {{ item.message }}
 </li> -->
