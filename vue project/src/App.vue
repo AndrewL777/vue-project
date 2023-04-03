41f29dacb72db380b573e7dc4cf37fd403561229
AndrewL777 <7landrew7@gmail.com>
2023-04-02 23:50:57 -0400

    2

--- vue project/src/App.vue
+++ vue project/src/App.vue
@@ -1,56 +1,53 @@
+<<<<<<< HEAD
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+=======
 <template>
 
-    <header> <button v-on:click="navigateTo('Pokemon You Chose')"  > View Team </button>
-      <button v-on:click="navigateTo('Pick Your Pokemon')"  > Choose Team </button> </header>
+    <header> <button v-on:click="navigateTo('cart')"  > View Team </button>
+      <button v-on:click="navigateTo('products')"  > Choose Team </button> </header>
  
  {{ page }}
-    <div v-if="page === 'Pokemon You Chose'">  
+    <div v-if="page === 'cart'">  
   <h1>Your Team</h1>
   <div class="Mons">
-      <div class="card" v-for="pokemon in team"
+      <div class="card" v-for="pokemon in cart"
         :key="pokemon.name">
         <p class="pokename">{{ pokemon.name }}</p>
         <div class="image"><img :src="pokemon.url" /></div>
-        <button v-on:click="RemovePokemonFromTeam(pokemon)" class="addTeam">
+        <button v-on:click="RemoveItemFromCart(pokemon)" class="addTeam">
          <h2>-</h2>
         </button>
       </div>
-    </div> </div>    
-
-
-<div v-if="page==='Pick Your Pokemon'"> 
-  <h1>Choose Pokemon</h1>  
-  <div class="Mons">
-      <div class="card" v-for="pokemon in pokemons"
-        :key="pokemon.name">
-        <p class="pokename">{{ pokemon.name }}</p>
-        <div class="image"><img :src="pokemon.url" /></div>
-        <button v-on:click="addItemToTeam(pokemon)" class="addTeam">
-         <h2>+</h2>
-        </button>
-      </div>
     </div> 
+  </div>    
+
+
+<div v-if="page==='products'"> 
+  <Mons />
+  
   </div>
 
 </template>
+
+
 <script>
-
-
-
+import Mons from  './views/ChooseThings.Vue';
 export default {
   name:"pokes",
   data() {
     return {
-      page: "Pick Your Pokemon",
-      team: [],
+      page: "products",
+      cart: [],
       pokemons: [
         {
           name: 'Lugia',
           url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/249.png'
         },
         {
-          name: 'Articuno',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/144.png'
+          name: 'Drowzee',
+          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/096.png'
         },
         {
           name: 'Tinkaton',
@@ -91,101 +88,44 @@ export default {
         {
           name: 'Garchomp',
           url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/445.png'
-        },
-        {
-          name: 'Lucario',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/448.png'
-        },
-        {
-          name: 'Dragonite',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/149.png'
-        },
-        {
-          name: 'Suicune',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/245.png'
-        },
-        {
-          name: 'Jolteon',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/135.png'
-        },
-        {
-          name: 'Salamance',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/373.png'
-        },
-        {
-          name: 'Kingambit',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/983.png'
-        },
-        {
-          name: 'Accelgor',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/617.png'
-        },
-        {
-          name: 'Darkrai',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/491.png'
-        },
-        {
-          name: 'Kyogre',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/382.png'
-        },
-        {
-          name: 'Kingdra',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/230.png'
-        },
-        {
-          name: 'Blastoise',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png'
-        },
-        {
-          name: 'Pikachu',
-          url: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png'
         }
-     
-       
-
       ]
     }
   },
   methods: {
-    addItemToTeam(pokemon){
-      this.team.push(pokemon);
-      console.log(this.team)
+    addItemToCart(pokemon){
+      this.cart.push(pokemon);
+      console.log(this.cart)
     },
     navigateTo(page) {
       this.page = page;
     },
-    RemovePokemonFromTeam(pokemon){
-      this.team.splice(this.team.indexOf(pokemon));
+    RemoveItemFromCart(pokemon){
+      this.cart.splice(this.cart.indexOf(pokemon));
     }
   }
 }
+>>>>>>> parent of 0237d6a (2)
 </script>
-<style>
-h1 {
-  text-align: center;
-}
-h2 {
-  font-size: 10px;
 
-}
-p{
-  text-align: center;
-  font-size: 20px;
-}
-.Mons {
-  margin-top: 100px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
-.card {
-  width: 300px;
-  height: 300px;
-  margin-top: 40px;
-  border: 3px solid black;
-}
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
+<<<<<<< HEAD
+    <div class="wrapper">
+      <HelloWorld msg="hi!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+=======
 .addTeam {
   width: 30px;
   height: 30px;
@@ -203,9 +143,4 @@ p{
   padding: 10px;
 }
 </style>
-<!-- make placeholders, use absolute positioning with ref,  -->
-
-<!-- data() { return { items: [{ message: 'Foo' },{ message: 'Bar' }] } } template
-<li v-for="item in items">
-  {{ item.message }}
-</li> -->
+>>>>>>> parent of 0237d6a (2)
